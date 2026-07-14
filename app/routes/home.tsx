5ef644ccb276a2299f8851a613b23332d7496907
muhd-jagfer <muhdjagfer@gmail.com>
2026-07-14 21:14:59 +0530

    navbar and basic ui completed

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,24 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumyzer" },
+    { name: "description", content: "Get instant feedback on your resume with our AI-powered analysis." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className = "bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+
+    <section className = "main-section">
+      <div className = "page-heading">
+        <h1> RESUMYZER </h1>
+        <h2> Track Your Resume & Review Your Submissions With AI Powered Feedback.</h2>
+      </div>
+    </section>
+
+    
+  </main>
 }
