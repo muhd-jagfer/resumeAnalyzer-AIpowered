4eac317e95573c0a46185c0e500aaf0e3ff06f04
muhd-jagfer <muhdjagfer@gmail.com>
2026-07-18 19:01:35 +0530

    score ui

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,8 +1,10 @@
 import {Link} from "react-router";
+import ScoreCircle from "./ScoreCircle";
 
-const ResumeCard = ({ resume: {id, companyName, jobTitle, feedback} }: { resume: Resume }) => {
+const ResumeCard = ({ resume: {id, companyName, jobTitle, feedback, resumeUrl} }: { resume: Resume }) => {
     return (
         <Link to= {`/resumes/${id}`} className = "resume-card animate-in fade-in duration-1000">
+        <div className="resume-card-header">
             <div className = "flex flex-col gap-2">
                 <h2 className = "!text-black font-bold break-words">
                     {companyName}
@@ -12,8 +14,18 @@ const ResumeCard = ({ resume: {id, companyName, jobTitle, feedback} }: { resume:
                 </h3>
             </div>
             <div className="flex-shrink-0">
-             
+                 <ScoreCircle score = {feedback.overallScore} />
             </div>
+        </div>
+        <div className ="gradient-border animate-in fade-in duration-1000">
+            <div className= "w-full h-full">
+                <img 
+                    src = {resumeUrl}
+                    alt = "resume"
+                    className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
+                    />
+            </div>
+        </div>
         </Link>
     )
 }
