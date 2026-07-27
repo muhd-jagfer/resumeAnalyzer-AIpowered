d9e7a0396b975cde9b7899c2e6af566da5f74eed
muhd-jagfer <muhdjagfer@gmail.com>
2026-07-27 20:43:08 +0530

    implementing auth

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
+import { useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
+import { useEffect } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,23 +14,33 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main className = "bg-[url('/images/bg-main.svg')] bg-cover">
-    <Navbar />
-    
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
 
-    <section className = "main-section">
-      <div className = "page-heading py-16">
-        <h1> RESUMYZER </h1>
-        <h2> Track Your Resume & Review Your Submissions With AI Powered Feedback.</h2>
-      </div>
+  useEffect(() => {
+    if (!auth.isAuthenticated) {
+      navigate("/auth?next=/");
+    }
+  }, [auth.isAuthenticated, navigate]);
 
-     {resumes.length > 0 && (
-      <div className = "resume-section">
-        {resumes.map((resume) => (
-            <ResumeCard key={resume.id} resume={resume} />
-        ))}
-      </div>
-      )}
-    </section>
-  </main>
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
+      <Navbar />
+
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>RESUMYZER</h1>
+          <h2>Track Your Resume & Review Your Submissions With AI Powered Feedback.</h2>
+        </div>
+
+        {resumes.length > 0 && (
+          <div className="resume-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
